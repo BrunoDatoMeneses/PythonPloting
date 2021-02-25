5f82bdaa3a4ea8bf035db7c29f2ae41311ef3610
BrunoDatoMeneses <bruno.dato.meneses@gmail.com>
2021-02-25 15:32:42 +0100

    XP: LL

--- _XP_LAUNCHER_MODELS_LL.py
+++ _XP_LAUNCHER_MODELS_LL.py
@@ -182,14 +182,14 @@ if __name__ == "__main__":
             learningCycles = [lrnCycles]
             launch()
 
-        isActiveExploitation = ["true"]
-
-        listOfEndoExploitationCycles = ["500","1000","2000","4000","6000","10000"]
-
-
-        for lrnCycles in listOfLearningCycles:
-            for endoExpCycles in listOfEndoExploitationCycles:
-                learningCycles = [lrnCycles]
-                endoExploitationCycles = [endoExpCycles]
-                launch()
+        # isActiveExploitation = ["true"]
+        #
+        # listOfEndoExploitationCycles = ["500","1000","2000","4000","6000","10000"]
+        #
+        #
+        # for lrnCycles in listOfLearningCycles:
+        #     for endoExpCycles in listOfEndoExploitationCycles:
+        #         learningCycles = [lrnCycles]
+        #         endoExploitationCycles = [endoExpCycles]
+        #         launch()
 
